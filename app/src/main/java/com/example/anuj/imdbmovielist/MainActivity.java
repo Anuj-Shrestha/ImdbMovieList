8c8d00c6a71116c46925a1def2c9b10225295258
Anuj-Shrestha <anujshrestha@lftechnology.com>
2017-04-24 14:20:06 +0545

    IML-1 Add search option

--- app/src/main/java/com/example/anuj/imdbmovielist/MainActivity.java
+++ app/src/main/java/com/example/anuj/imdbmovielist/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.anuj.imdbmovielist;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
 import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.ListView;
@@ -19,6 +21,9 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private ListView listView;
+    private ImageButton imageButton;
+    private EditText editText;
+    private String searchQuery;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -26,8 +31,38 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         listView = (ListView) findViewById(R.id.listview_githubrepos);
+        imageButton = (ImageButton) findViewById(R.id.imagebutton_search);
+        editText = (EditText) findViewById(R.id.edittext_search);
+
+        imageButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                editText.setVisibility(View.VISIBLE);
+
+            }
+        });
+
+        editText.setOnKeyListener(new View.OnKeyListener() {
+            public boolean onKey(View v, int keyCode, KeyEvent event) {
+                // If the event is a key-down event on the "enter" button
+                if ((event.getAction() == KeyEvent.ACTION_DOWN) &&
+                        (keyCode == KeyEvent.KEYCODE_ENTER)) {
+                    // Perform action on key press
+                    searchQuery = editText.getText().toString();
+                    CallRetrofit(searchQuery);
+                    return true;
+                }
+                return false;
+            }
+        });
+
+        searchQuery = "a";
+        CallRetrofit(searchQuery);
 
 
+    }
+
+    public void CallRetrofit(String searchParam) {
         Retrofit.Builder builder = new Retrofit.Builder()
                 .baseUrl("http://www.omdbapi.com")
                 .addConverterFactory(GsonConverterFactory.create());
@@ -37,7 +72,7 @@ public class MainActivity extends AppCompatActivity {
         // Creating an object of our api interface
         ImdbClient client = retrofit.create(ImdbClient.class);
 
-        Call<MovieResponse> call = client.reposForUser("hulk");
+        Call<MovieResponse> call = client.reposForUser(searchParam);
 
         call.enqueue(new Callback<MovieResponse>() {
             @Override
